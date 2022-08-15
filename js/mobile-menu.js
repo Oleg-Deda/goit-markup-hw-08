abf56208e74a054d679507ec2100b78cf9bd4468
Oleg-Deda <106027183+Oleg-Deda@users.noreply.github.com>
2022-08-15 07:07:32 +0300

    мобильное меню продолжаю делать

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -13,16 +13,38 @@
 //     document.body.classList.toggle('mobile-menu-open');
 //   });
 // })();
+
+// RUS
+
+// (() => {
+//   const menuBtnRef = document.querySelector('[data-menu-button]');
+//   const mobileMenuRef = document.querySelector('[data-menu]');
+//   const mobileBtnClose = document.querySelector('[data-menu-close]');
+
+//   menuBtnRef.addEventListener('click', () => {
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+
+//   mobileBtnClose.addEventListener('click', () => {
+//     mobileMenuRef.classList.toggle('is-open');
+//   });
+// })();
+
+// RUS
+
+// KIRIL
 (() => {
   const menuBtnRef = document.querySelector('[data-menu-button]');
   const mobileMenuRef = document.querySelector('[data-menu]');
-  const mobileBtnClose = document.querySelector('[data-menu-close]');
 
   menuBtnRef.addEventListener('click', () => {
-    mobileMenuRef.classList.toggle('is-open');
-  });
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
 
-  mobileBtnClose.addEventListener('click', () => {
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
     mobileMenuRef.classList.toggle('is-open');
+
+    document.body.classList.toggle('mobile-menu-open');
   });
 })();
